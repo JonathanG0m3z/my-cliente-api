5d89979df409c31ff941d2c39137b0031f5390c0
JonathanG0m3z <104044651+JonathanG0m3z@users.noreply.github.com>
2023-04-01 17:56:35 -0500

    database configuration

---
+++ models/index.js
@@ -0,0 +1,42 @@
+const dbConfig = require('@config/dbConfig.js');
+const {Sequelize, DataTypes} = require('sequelize');
+
+const sequelize = new Sequelize(
+    dbConfig.DB,
+    dbConfig.USER,
+    dbConfig.PASSWORD, {
+        host: dbConfig.HOST,
+        dialect: dbConfig.dialect,
+        operatorsAliases: false,
+
+        pool: {
+            max: dbConfig.pool.max,
+            min: dbConfig.pool.min,
+            acquire: dbConfig.pool.acquire,
+            idle: dbConfig.pool.idle,
+        }
+    }
+);
+
+sequelize.authenticate()
+.then(() => {
+    console.log("Connected...");
+})
+.catch(err => {
+    console.log("Error "+err);
+})
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.products = require('./productModel.js')(sequelize, DataTypes);
+
+db.sequelize.sync({ force: true })
+.then(() => {
+    console.log("ReSync done!");
+})
+.catch(err =>{
+    console.log("Error: "+err);
+})
