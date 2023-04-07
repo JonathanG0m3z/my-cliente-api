3abad8a8c37f988d2639dace0459297d513b153e
JonathanG0m3z <104044651+JonathanG0m3z@users.noreply.github.com>
2023-04-07 10:02:39 -0500

    accounts crud

--- routes/accountRouter.js
+++ routes/accountRouter.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const accountController = require('../controllers/accountController');
 const verifyToken = require('../middlewares/jwtMiddleware');
 
-// router.get('/', verifyToken, accountController.getPrices);
+router.get('/', verifyToken, accountController.getAccounts);
 router.post('/', verifyToken, accountController.addAccount);
-// router.get('/:id', verifyToken, accountController.getPriceById);
-// router.put('/:id', verifyToken, accountController.updatePrice);
-// router.delete('/:id', verifyToken, accountController.deletePrice);
+router.get('/:id', verifyToken, accountController.getAccountById);
+router.put('/:id', verifyToken, accountController.updateAccount);
+router.delete('/:id', verifyToken, accountController.deleteAccount);
 
 module.exports = router;
